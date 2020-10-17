b111b9f46e2a833de26979ed074b4b7633e4faf3
MouseProducedGames <largemouse@gmail.com>
2020-10-17 12:05:23 -0700

    Removed asserts; made the tile_type to char match work with `Option<TileType>`.

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,13 @@ fn draw_map(map_id: MapId) {
     for y in map.area().position().y()..=map.area().bottom() {
         for x in map.area().position().x()..=map.area().right() {
             let tile_type = map.tile_type_at_local(Position::new(x, y));
-            if tile_type.is_none() {
-                continue;
-            }
 
-            let tile_type = tile_type.unwrap();
             let ch = match tile_type {
-                TileType::Void => ' ',
-                TileType::Floor => '.',
-                TileType::Wall => '#',
-                TileType::Portal => '+',
+                Some(TileType::Void) => ' ',
+                Some(TileType::Floor) => '.',
+                Some(TileType::Wall) => '#',
+                Some(TileType::Portal) => '+',
+                None => ' ',
             };
 
             print!("{}", ch);
@@ -43,16 +40,13 @@ fn draw_placed_map(map_id: MapId) {
     for y in map.area().position().y()..=map.area().bottom() {
         for x in map.area().position().x()..=map.area().right() {
             let tile_type = map.tile_type_at_local(Position::new(x, y));
-            if tile_type.is_none() {
-                continue;
-            }
 
-            let tile_type = tile_type.unwrap();
             let ch = match tile_type {
-                TileType::Void => ' ',
-                TileType::Floor => '.',
-                TileType::Wall => '#',
-                TileType::Portal => '+',
+                Some(TileType::Void) => ' ',
+                Some(TileType::Floor) => '.',
+                Some(TileType::Wall) => '#',
+                Some(TileType::Portal) => '+',
+                None => ' ',
             };
 
             print!("{}", ch);
@@ -72,11 +66,11 @@ fn main() {
     // This CountRange will generate a number in the range [4, 9].
     let num_sub_maps = CountRange::new(4, 9).provide_count();
     let map_id = DunGen::new(SparseMap::new())
-        .gen_with(FillTilesGenerator::new(Size::new(40, 30), TileType::Void))
+        .gen_with(FillTilesGenerator::new(Size::new(60, 30), TileType::Void))
         .gen_with(SubMapGenerator::new(
             &[SubMapGeneratorSet::new(
                 &num_sub_maps,
-                &Area::new(Position::new(0, 0), Size::new(40, 30)),
+                &Area::new(Position::new(0, 0), Size::new(48, 18)),
                 Some(Box::new(SparseMap::new)),
                 Some(&[&EmptyRoomGenerator::new(SizeRange::new(
                     Size::new(6, 6),
@@ -87,24 +81,6 @@ fn main() {
             Some(&[&WalledRoomGenerator::new(Size::zero())]),
         ))
         .build();
-
-    let maps = MAPS.read();
-    let map = maps[map_id].read();
-
-    assert!(*map.size() == Size::new(40, 30));
-    assert!(map.sub_map_count() == num_sub_maps);
-    assert!(map.sub_map_count() >= 4 && map.sub_map_count() <= 9);
-    let mut sub_map_count = 0;
-    for sub_map in map.sub_maps() {
-        let target_map = maps[sub_map.value()].read();
-        assert!(target_map.size().width() >= 6 && target_map.size().width() <= 12);
-        assert!(target_map.size().height() >= 6 && target_map.size().height() <= 12);
-        assert!(target_map.tile_type_at_local(Position::new(0, 0)) == Some(TileType::Wall));
-        assert!(target_map.tile_type_at_local(Position::new(1, 1)) == Some(TileType::Floor));
-        sub_map_count += 1;
-    }
-    assert!(sub_map_count == num_sub_maps);
-    assert!(sub_map_count >= 4 && sub_map_count <= 9);
 
     println!("Map");
     draw_map(map_id);
