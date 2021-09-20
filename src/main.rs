d6977f39481d79dc8f534f829f25168413fd4626
Bruce Farnell <largemouse@gmail.com>
2021-09-20 13:50:26 -0700

    Various checks updated to work with latest.

--- src/main.rs
+++ src/main.rs
@@ -79,7 +79,7 @@ fn draw_placed_map(map_id: MapId, drawn: &mut HashSet<MapId>) {
 }
 
 fn main() {
-    /* let values: Box<[(Inclusion, Box<dyn PlacedShape>); 2]> = Box::new([
+    let values: Box<[(Inclusion, Box<dyn PlacedShape>); 2]> = Box::new([
         (
             Inclusion::Include,
             Box::new(Oval::new(Position::new(0, 0), Size::new(22, 22))),
@@ -93,15 +93,14 @@ fn main() {
         ),
     ]);
 
-    let its_a_big_plus = PlacedShapeSlice::new(values);
+    let its_a_big_donut = PlacedShapeSlice::new(values);
 
     let map_id = DunGen::new(SparseMap::new())
-    .gen_with(EmptyRoomGenerator::new(its_a_big_plus.clone()))
-    .gen_with(WalledRoomGenerator::new(its_a_big_plus))
-    .build(); */
+        .gen_with(&EmptyRoomGenerator::new(its_a_big_donut.clone()))
+        .gen_with(&WalledRoomGenerator::new(its_a_big_donut))
+        .build();
 
-    /*
-    let values: Box<[(Inclusion, Box<dyn PlacedShape>); 2]> = Box::new([
+    /* let values: Box<[(Inclusion, Box<dyn PlacedShape>); 2]> = Box::new([
         (
             Inclusion::Include,
             Box::new(Area::new(Position::new(2, 0), Size::new(5, 9))),
@@ -115,30 +114,27 @@ fn main() {
     let its_a_big_plus = PlacedShapeSlice::new(values);
 
     let map_id = DunGen::new(SparseMap::new())
-        .gen_with(EmptyRoomGenerator::new(its_a_big_plus.clone()))
-        .gen_with(WalledRoomGenerator::new(its_a_big_plus))
-        .build();
-    */
-
-    let map_id = DunGen::new(SparseMap::new())
-        .gen_with(&EmptyRoomGenerator::new(Oval::new(
-            Position::new(0, 0),
-            Size::new(22, 22),
-        )))
-        .gen_with(&WalledRoomGenerator::new(Oval::new(
-            Position::new(0, 0),
-            Size::new(22, 22),
-        )))
-        .build();
+        .gen_with(&EmptyRoomGenerator::new(its_a_big_plus.clone()))
+        .gen_with(&WalledRoomGenerator::new(its_a_big_plus))
+        .build(); */
 
     /* let map_id = DunGen::new(SparseMap::new())
-        .gen_with(EmptyRoomGenerator::new(Size::new(10, 10)))
-        .gen_with(WalledRoomGenerator::new(Size::zero()))
-        .build();
+    .gen_with(&EmptyRoomGenerator::new(Oval::new(
+        Position::new(0, 0),
+        Size::new(22, 22),
+    )))
+    .gen_with(&WalledRoomGenerator::new(Oval::new(
+        Position::new(0, 0),
+        Size::new(22, 22),
+    )))
+    .build(); */
 
-    draw_map(map_id, &mut HashSet::new());
+    /* let map_id = DunGen::new(SparseMap::new())
+    .gen_with(&EmptyRoomGenerator::new(Size::new(10, 10)))
+    .gen_with(&WalledRoomGenerator::new(Size::zero()))
+    .build(); */
 
-    {
+    /* {
         let maps = MAPS.read();
         let mut map = maps[map_id].write();
         for y in 4..=5 {
@@ -146,26 +142,23 @@ fn main() {
                 map.tile_type_at_local_set(Position::new(x, y), TileType::Void);
             }
         }
-    }
+    } */
 
-    draw_map(map_id, &mut HashSet::new());
+    /* let map_id = DunGen::new(map_id)
+    .gen_with(&WalledRoomGenerator::new(Size::zero()))
+    .build(); */
 
-    let map_id = DunGen::new(map_id)
-        .gen_with(WalledRoomGenerator::new(Size::zero()))
-        .build(); */
-
-    /*
-    let num_portals = CountRange::new(2, 5).provide_count();
+    /* let num_portals = CountRange::new(2, 5).provide_count();
     let map_id = DunGen::new(SparseMap::new())
-        .gen_with(SequentialGenerator::new(&[
+        .gen_with(&SequentialGenerator::new(&[
             &EmptyRoomGenerator::new(Size::new(12, 8)),
             &EdgePortalsGenerator::new(num_portals, Box::new(SparseMap::new)),
             // &EdgePortalsGenerator::new(1, Box::new(SparseMap::new)),
         ]))
-        .gen_with(TraversePortalsGenerator::new(EmptyRoomGenerator::new(
+        .gen_with(&TraversePortalsGenerator::new(EmptyRoomGenerator::new(
             Size::new(8, 6),
         )))
-        .gen_with(TraversePortalsGenerator::new(IfMapThenGenerator::new(
+        .gen_with(&TraversePortalsGenerator::new(IfMapThenGenerator::new(
             |map_id| {
                 let maps = MAPS.read();
                 let map = maps[map_id].read();
@@ -174,7 +167,7 @@ fn main() {
             EdgePortalsGenerator::new(CountRange::new(2, 5), Box::new(SparseMap::new)),
             // EdgePortalsGenerator::new(1, Box::new(SparseMap::new)),
         )))
-        .gen_with(TraversePortalsGenerator::new(IfMapThenGenerator::new(
+        .gen_with(&TraversePortalsGenerator::new(IfMapThenGenerator::new(
             |map_id| {
                 let maps = MAPS.read();
                 let map = maps[map_id].read();
@@ -182,23 +175,21 @@ fn main() {
             },
             EmptyRoomGenerator::new(SizeRange::new(Size::new(6, 6), Size::new(12, 12))),
         )))
-        .gen_with(TraverseThisAndPortalsGenerator::new(
+        .gen_with(&TraverseThisAndPortalsGenerator::new(
             SequentialGenerator::new(&[
                 &WalledRoomGenerator::new(Size::zero()),
                 &ReciprocatePortalsGenerator::new(),
             ]),
         ))
-        .gen_with(MergePortalMapsAsSubMapsGenerator::new(2, |_| true))
-        .gen_with(FillTilesGenerator::new(
+        .gen_with(&MergePortalMapsAsSubMapsGenerator::new(2, |_| true))
+        .gen_with(&FillTilesGenerator::new(
             Oval::new(Position::new(7, 7), Size::new(10, 10)),
             TileType::Void,
         ))
-        .gen_with(WalledRoomGenerator::new(InvertPlacedShape::new(Oval::new(
-            Position::new(7, 7),
-            Size::new(10, 10),
-        ))))
-        .build();
-    */
+        .gen_with(&WalledRoomGenerator::new(InvertPlacedShape::new(
+            Oval::new(Position::new(7, 7), Size::new(10, 10)),
+        )))
+        .build(); */
 
     draw_map(map_id, &mut HashSet::new());
 }
